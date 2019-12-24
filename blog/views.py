46725957f88aea1e9ff8aa0f1702dc6cf82b1675
Amine Zitoun <zitoun.amino@yahoo.com>
2019-12-24 18:42:17 +0100

    pst

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+# Create your views here.
+
+
+
+
+def index(request):
+    return render(request,'blog/index.html')
+def about(request):
+    return render(request,'blog/about.html')
+def projects(request):
+
+    return render(request,'blog/projects.html')
